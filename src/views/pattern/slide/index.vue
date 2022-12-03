da7e700b15798e8a78b74ec89cfea4b87aefc579
linj <457190152@qq.com>
2022-12-03 15:37:48 +0800

    linj 12/3

---
+++ src/views/pattern/slide/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sider-main"></div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+.sider-main{
+  min-height: calc(100vh - 84px);
+  width: 100%;
+  background-color: #afc;
+}
+
+</style>

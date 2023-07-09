29cce506e48aea7c705b4c7884082e506253c31c
study-till-night <zdscg@sina.com>
2023-07-09 20:29:32 +0800

    实例页面初始化

--- src/views/system/Instance.vue
+++ src/views/system/Instance.vue
@@ -1,7 +1,79 @@
 <template>
-  <div>entity</div>
+  <div class="top-content">
+    <h5>构建实例</h5>
+  </div>
+  <div class="main-content">
+    <el-form :model="attrForm" label-width="120px">
+      <el-form-item label="实例本体源">
+        <el-select
+          v-model="ontoId"
+          placeholder="请选择实例所属本体源"
+          size="normal"
+          @change="handleSelChange"
+          clearable
+        >
+          <el-option
+            v-for="(item, index) in ontoList"
+            :key="index"
+            :label="item"
+            :value="item"
+          />
+        </el-select>
+      </el-form-item>
+      <!-- 属性输入框 -->
+      <el-form-item
+        v-for="(item, index) in attrForm"
+        :label="`请输入${item.name}`"
+      >
+        <el-input
+          v-model="attrForm[index].value"
+          :placeholder="`请输入${item.name}`"
+          clearable
+        />
+      </el-form-item>
+      <!-- 提交按钮 -->
+      <el-form-item>
+        <el-button type="success" @click="handleInsCreate">创建实例</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { add } from "@/api/module/instance.js";
+import { queryOntolist } from "@/api/module/ontology.js";
+import { reactive, ref } from "vue";
 
-<style lang="scss" scoped></style>
+const ontoId = ref("");
+const attrForm = reactive([]);
+const ontoList = reactive([]);
+
+for (let i = 0; i < 10; i++) {
+  attrForm.push({ name: "属性" + i, value: "" });
+}
+
+// 获取本体列表
+
+// 本体选择改变
+const handleSelChange = () => {};
+// 处理提交
+const handleInsCreate = () => {};
+</script>
+
+<style lang="less" scoped>
+@import url("../../assets/global.less");
+
+@length: 130px;
+.main-content {
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+
+  .el-form {
+    width: 30%;
+    .el-button {
+      margin-left: @length;
+    }
+  }
+}
+</style>
